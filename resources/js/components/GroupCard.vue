f6d58ad600e76a400d9a11bd2f97033a7ef95f72
rootcause0 <caracobiceps@yandex.com>
2022-06-17 15:16:19 +0300

    first commit

---
+++ resources/js/components/GroupCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <a
+        class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
+        <img class="object-cover w-10 h-10 rounded-full"
+             src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
+             alt="username"/>
+        <div class="w-full pb-2">
+            <div class="flex justify-between">
+                <span class="block ml-2 font-semibold text-gray-600">{{ name }}</span>
+                <span class="block ml-2 text-sm text-gray-600">{{ onlineCount }} online</span>
+            </div>
+            <span class="block ml-2 text-sm text-gray-600">{{ lastMessage }}</span>
+        </div>
+    </a>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        lastMessage: String,
+        onlineCount : Number
+    }
+}
+</script>
+
